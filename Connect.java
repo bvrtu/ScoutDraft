7b04c90723d446c2360cd610b6b543d2287ef6d4
hsnefe <119686500+hsnefe@users.noreply.github.com>
2024-12-04 14:51:45 +0300

    create table part1.
    In the database created a table's part.
    Later on, there will be changes in the create table method.

--- Connect.java
+++ Connect.java
@@ -1,6 +1,10 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.*;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Connect {
     /**
@@ -13,7 +17,6 @@ public class Connect {
             String url = "jdbc:sqlite:database.db";
             // create a connection to the database
             conn = DriverManager.getConnection(url);
-
             System.out.println("Connection to SQLite has been established.");
 
         } catch (SQLException e) {
@@ -28,10 +31,39 @@ public class Connect {
             }
         }
     }
+    public static void create_table() {
+        try{
+            var conn = DriverManager.getConnection("jdbc:sqlite:database.db");
+            var stmt = conn.createStatement();
+            var create = "CREATE TABLE IF NOT EXISTS (" +
+                    "id INTEGER PRIMARY KEY," +
+                    "Name NOT NULL," +
+                    "Club NOT NULL," +
+                    "CA INTEGER," +
+                    "Height INTEGER," +
+                    "Nation NOT NULL," +
+                    "Foot NOT NULL," +
+                    "";
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         connect();
+
+      /*  Scanner sc = new Scanner(new File("fm24eng.csv"));
+        ArrayList<?> collumn = new ArrayList<>();
+        for(int i = 1;sc.hasNextLine();i++){
+            String row = sc.nextLine();
+            for(String j: row.split(";")){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+       */
     }
 }
